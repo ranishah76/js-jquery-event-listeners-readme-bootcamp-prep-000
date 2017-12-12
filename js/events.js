3084c311a9ad014e7ac660777cb83119b163897e
Rani <rani76@gmail.com>
2017-12-12 11:55:16 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
 
-// call functions here
+function frameIt () {
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
 
-});
+function pressIt() {
+  $("form").on('keydown', function (key){
+    if(key.which == 71) {
+      alert ('G was pressed')
+    }
+  })
+}
+
+function submitIt () {
+  $('form').on("submit", function (key){
+    alert("Your form is going to be submitted now.")
+      })
+}
