2c3a94e946b90539cc61d59f9cfd8e1c6ff58300
Rani <rani76@gmail.com>
2017-12-12 11:51:03 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,3 +5,30 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
+
+function frameIt () {
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $("form").on('keydown', function (key){
+    if(key.which == 71) {
+      alert ('G was pressed')
+    }
+  })
+}
+
+function SubmitIt () {
+  $("form").on("submit", function (){
+    alert('Your form is going to be submitted now')
+    return
+  })
+}
